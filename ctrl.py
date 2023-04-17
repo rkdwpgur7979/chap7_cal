004ba6e9e56ba34dc8a0e0289a145b019a4e6924
D416-04\user <rkdwpgur7979@stu.jejunu.ac.kr>
2023-04-17 14:12:03 +0900

    Modify cal function in ctrl.py

--- ctrl.py
+++ ctrl.py
@@ -4,14 +4,27 @@ class Control:
         self.connectSignals()
         
     def calculate(self):
-        num1 = float(self.view.le1.text())
-        num2 = float(self.view.le2.text())
-        operator =self.view.cb.currentText()
+        try:
+            num1 = float(self.view.le1.text())
+            num2 = float(self.view.le2.text())
+            operator =self.view.cb.currentText()
         
-        if operator =='+':
-            return f'{num1} + {num2} = {self.sum(num1, num2)}'
+            if operator == '+':
+                return f'{num1} + {num2} = {self.sum(num1, num2)}'
+            elif operator == '-':
+                return f'{num1} - {num2} = {self.sub(num1, num2)}'
+            elif operator == '*':
+                return f'{num1} * {num2} = {self.mul(num1, num2)}'
+            elif operator == '/':
+                return f'{num1} / {num2} = {self.div(num1, num2)}'
+            elif operator == '^':
+                return f'{num1} ^ {num2} = {self.pow(num1, num2)}'
+            elif operator == '%':
+                return f'{num1} % {num2} = {self.mod(num1, num2)}'
+            else :
+                return "Calculation Error"
         
-        else:
+        except:
             return "Calculation Error"
         
     def connectSignals(self):
@@ -28,23 +41,18 @@ class Control:
         return a*b
     
     def div(self, a, b):
+        try:
+            if(b==0):
+                raise Exception("Divisor Error")
+            
+        except Exception as e:
+            return e
+        
         return a/b
     
     def pow(self, a, b):
         try:
            if (a==0):
-<<<<<<< HEAD
-<<<<<<< HEAD
-                raise Exception("Base Error")
-            
-        except Exception as e:
-            return e
-        
-        return pow(a, b)   
-=======
-                print("어머같다")
-=======
->>>>>>> bae38f5 (Modify pow func using exception)
                 raise Exception("Base Error")
             
         except Exception as e:
@@ -52,4 +60,13 @@ class Control:
         
         return pow(a, b)   
     
->>>>>>> ce3e0bb (Modify pow func using check base)
+    def mod(self, a, b):
+        try:
+            if(b==0):
+                raise Exception("Divisor Error")
+            
+        except Exception as e:
+            return e
+        
+        return a%b
+    
